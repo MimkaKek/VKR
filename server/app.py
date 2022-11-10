4aaf0ec78ca0b7d6f50c74c59e33231a539b8374
MimkaKek <grislava@gmail.com>
2022-11-10 14:33:34 +0300

    Port setup

--- server/app.py
+++ server/app.py
@@ -1,24 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-BOOKS = [
-    {
-        'title': 'On the Road',
-        'author': 'Jack Kerouac',
-        'read': True
-    },
-    {
-        'title': 'Harry Potter and the Philosopher\'s Stone',
-        'author': 'J. K. Rowling',
-        'read': False
-    },
-    {
-        'title': 'Green Eggs and Ham',
-        'author': 'Dr. Seuss',
-        'read': True
-    }
-]
-
 # configuration
 DEBUG = True
 
@@ -28,13 +10,6 @@ app.config.from_object(__name__)
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
-
-@app.route('/books', methods=['GET'])
-def all_books():
-    return jsonify({
-        'status': 'success',
-        'books': BOOKS
-    })
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
