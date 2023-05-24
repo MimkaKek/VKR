30f2786eb11454b65aee6534418b919020322dd2
MimkaKek <33195586+MimkaKek@users.noreply.github.com>
2023-05-24 10:34:39 +0300

    Add try

--- server/src/app/routes/projectRoutes.py
+++ server/src/app/routes/projectRoutes.py
@@ -13,276 +13,361 @@ projectBlueprint = Blueprint('project_blueprint', __name__)
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def templatesAllList():
-    pManager = ProjectManager()
-    callback = pManager.GetTemplatesWithNames()
-    return jsonify(callback.dict())
+    try:
+        pManager = ProjectManager()
+        callback = pManager.GetTemplatesWithNames()
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/templates', methods=['GET'])
 @cross_origin()
 @loginRequired(cfg.ROLES.TEACHER)
 def templatesList():
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
 
-    callback = pManager.GetUserProjects(username, isTemplate=True)
-    return jsonify(callback.dict())
+        callback = pManager.GetUserProjects(username, isTemplate=True)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/public_projects', methods=['GET'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def publicProjects():
-    pManager = ProjectManager()
-    callback = pManager.GetAllProjects()
-    return jsonify(callback.dict())
+    try:
+        pManager = ProjectManager()
+        callback = pManager.GetAllProjects()
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/projects', methods=['GET'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def projectsList():
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
 
-    callback = pManager.GetUserProjects(username)
-    return jsonify(callback.dict())
+        callback = pManager.GetUserProjects(username)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/projects', methods=['PUT'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def projectsCreate():
+    try:
+        sid = request.args.get('sid', None)
 
-    sid = request.args.get('sid', None)
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
+        
+        if "data" not in request.json:
+            logger.warn("'data' field needed")
+            return jsonify(Callback(status=1, description="'data' field required").dict())
+        if "meta" not in request.json["data"]:
+            logger.warn("'meta' field needed")
+            return jsonify(Callback(status=1, description="'meta' field required").dict())
+        
+        meta = ProjectData()
+        meta.__dict__.update(request.json["data"]["meta"])
+        meta.owner = username
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
-    
-    if "data" not in request.json:
-        logger.warn("'data' field needed")
-        return jsonify(Callback(status=1, description="'data' field required").dict())
-    if "meta" not in request.json["data"]:
-        logger.warn("'meta' field needed")
-        return jsonify(Callback(status=1, description="'meta' field required").dict())
-    
-    meta = ProjectData()
-    meta.__dict__.update(request.json["data"]["meta"])
-    meta.owner = username
+        templateID = None
+        if "tid" in request.json["data"]:
+            templateID = request.json["data"]["tid"]
 
-    templateID = None
-    if "tid" in request.json["data"]:
-        templateID = request.json["data"]["tid"]
-
-    callback = pManager.CreateProject(username, meta, templateID)
-    return jsonify(callback.dict())
+        callback = pManager.CreateProject(username, meta, templateID)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/project/<hash>', methods=['GET'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def projectGet(hash):
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
-    
-    callback = pManager.GetProject(username, hash)
-    return jsonify(callback.dict())
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
+        
+        callback = pManager.GetProject(username, hash)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/project/<hash>', methods=['PATCH'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def projectUpdate(hash):
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
-    
-    if "data" not in request.json:
-        logger.warn("'data' field needed")
-        return jsonify(Callback(status=1, description="'data' field required").dict())
-    data = request.json["data"]
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
+        
+        if "data" not in request.json:
+            logger.warn("'data' field needed")
+            return jsonify(Callback(status=1, description="'data' field required").dict())
+        data = request.json["data"]
 
-    if "isTemplate" in data:
-        roleStatus = uManager.GetRole(user)
-        if roleStatus.status != 0:
-            return jsonify(roleStatus.dict())
-        role = roleStatus.data
+        if "isTemplate" in data:
+            roleStatus = uManager.GetRole(user)
+            if roleStatus.status != 0:
+                return jsonify(roleStatus.dict())
+            role = roleStatus.data
 
-        if role == cfg.ROLES.STUDENT:
-            logger.warn("Attempt to change some specific meta for project {pid} by user {name}".format(pid=hash, name=username))
-            data.pop("isTemplate")
+            if role == cfg.ROLES.STUDENT:
+                logger.warn("Attempt to change some specific meta for project {pid} by user {name}".format(pid=hash, name=username))
+                data.pop("isTemplate")
 
-    pManager = ProjectManager()
-    callback = pManager.SetProjectMeta(username, hash, data)
-    return jsonify(callback.dict())
+        pManager = ProjectManager()
+        callback = pManager.SetProjectMeta(username, hash, data)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/project/<hash>', methods=['DELETE'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def projectRemove(hash):
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
-    
-    callback = pManager.RemoveProject(username, hash)
-    return jsonify(callback.dict())
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
+        
+        callback = pManager.RemoveProject(username, hash)
+        logger.debug(jsonify(callback.dict()))
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/project/<hash>', methods=['POST'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def projectCopy(hash):
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
 
-    callback = pManager.CopyProject(username, hash)
-    return jsonify(callback.dict())
+        callback = pManager.CopyProject(username, hash)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/project/<hash>/<filename>', methods=['GET'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def projectFileGet(hash, filename):
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
-    
-    callback = pManager.GetFileProject(username, hash, filename)
-    return jsonify(callback.dict())
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
+        
+        callback = pManager.GetFileProject(username, hash, filename)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/project/<hash>/<filename>', methods=['PATCH'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def projectFileSet(hash, filename):
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
-    
-    if "data" not in request.json:
-        logger.warn("'data' field needed")
-        return jsonify(Callback(status=1, description="'data' field required").dict())
-    data     = request.json["data"]
-    
-    callback = pManager.SetFileProject(username, hash, filename, data)
-    return jsonify(callback.dict())
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
+        
+        if "data" not in request.json:
+            logger.warn("'data' field needed")
+            return jsonify(Callback(status=1, description="'data' field required").dict())
+        data     = request.json["data"]
+        
+        callback = pManager.SetFileProject(username, hash, filename, data)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/project/<hash>/<filename>', methods=['DELETE'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def projectFileDelete(hash, filename):
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
-    
-    callback = pManager.RemoveProjectFile(username, hash, filename)
-    return jsonify(callback.dict())
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
+        
+        callback = pManager.RemoveProjectFile(username, hash, filename)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/project/<hash>/<filename>', methods=['PUT'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def projectFileCreate(hash, filename):
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
-    logger.debug("CHECK 2")
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
-    
-    callback = pManager.CreateProjectFile(username, hash, filename)
-    return jsonify(callback.dict())
+        uManager = UserManager()
+        pManager = ProjectManager()
+        logger.debug("CHECK 2")
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
+        
+        callback = pManager.CreateProjectFile(username, hash, filename)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/project/<hash>/<filename>/preview', methods=['GET'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def projectFilePreview(hash, filename):
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
-    
-    callback = pManager.GeneratePage(username, hash, filename)
-    return callback.data
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
+        
+        callback = pManager.GeneratePage(username, hash, filename)
+        return callback.data
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/link/<hash>', methods=['GET'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def getShareLink(hash):
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
-    
-    callback = pManager.GetLinkProject(username, hash)
-    return jsonify(callback.dict())
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
+        
+        callback = pManager.GetLinkProject(username, hash)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/link/<hash>', methods=['PUT'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def createShareLink(hash):
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
-    
-    callback = pManager.CreateLinkProject(username, hash)
-    return jsonify(callback.dict())
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
+        
+        callback = pManager.CreateLinkProject(username, hash)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @projectBlueprint.route('/link/<hash>', methods=['PATCH'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def useShareLink(hash):
-    sid = request.args.get('sid', None)
+    try:
+        sid = request.args.get('sid', None)
 
-    uManager = UserManager()
-    pManager = ProjectManager()
+        uManager = UserManager()
+        pManager = ProjectManager()
 
-    user     = uManager.GetUser(sid=sid).data
-    username = user.username
-    
-    callback = pManager.UseLinkProject(username, hash)
-    return jsonify(callback.dict())
+        user     = uManager.GetUser(sid=sid).data
+        username = user.username
+        
+        callback = pManager.UseLinkProject(username, hash)
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
--- server/src/app/routes/userRoutes.py
+++ server/src/app/routes/userRoutes.py
@@ -13,109 +13,118 @@ userBlueprint = Blueprint('user_blueprint', __name__)
 @userBlueprint.route('/user', methods=['POST'])
 @cross_origin()
 def auth():
-    logger.info("Auth(): begin")
-    logger.debug(request.args)
-    email    = request.args.get("mail", None)
-    username = request.args.get("name", None)
-    password = request.args.get("pass", "")
+    try:
+        email    = request.args.get("mail", None)
+        username = request.args.get("name", None)
+        password = request.args.get("pass", "")
 
-    mgr = UserManager()
-    logger.debug("Params auth: {mail} | {name} | {passwd}".format(mail=email, name=username, passwd=password))
-    userStatus = mgr.GetUser(username, email)
-    if userStatus.status != 0:
-        return jsonify(userStatus.dict())
-    user = userStatus.data
+        mgr = UserManager()
+        logger.debug("Params auth: {mail} | {name} | {passwd}".format(mail=email, name=username, passwd=password))
+        userStatus = mgr.GetUser(username, email)
+        if userStatus.status != 0:
+            return jsonify(userStatus.dict())
+        user = userStatus.data
 
-    authStatus = mgr.ValidateUser(user, password)
-    if authStatus.status != 0:
-        return jsonify(authStatus.dict())
-    
-    sidStatus = mgr.CreateSID(user)
-    if sidStatus.status != 0:
-        return jsonify(authStatus.dict())
+        authStatus = mgr.ValidateUser(user, password)
+        if authStatus.status != 0:
+            return jsonify(authStatus.dict())
+        
+        sidStatus = mgr.CreateSID(user)
+        if sidStatus.status != 0:
+            return jsonify(authStatus.dict())
 
-    logger.debug("New SID {sid} for user {name} with role {role}".format(sid=user.sid, name=user.username, role=user.role))
+        logger.debug("New SID {sid} for user {name} with role {role}".format(sid=user.sid, name=user.username, role=user.role))
 
-    callback = Callback(data={"sid": user.sid, "role": user.role})
-    return jsonify(callback.dict())
+        callback = Callback(data={"sid": user.sid, "role": user.role})
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @userBlueprint.route('/user', methods=['PUT'])
 @cross_origin()
 def register():
-    logger.debug("Register user: begin")
+    try:
+        username = request.args.get('name')
+        email    = request.args.get('mail')
+        password = request.args.get('pass')
 
-    username = request.args.get('name')
-    email    = request.args.get('mail')
-    password = request.args.get('pass')
+        if username is None or email is None or password is None:
+            return jsonify(Callback(status=1, description="Some fields required!").dict())
+        
+        usrManager = UserManager()
+        callback   = usrManager.AddUser(username, email, password)
 
-    if username is None or email is None or password is None:
-        return jsonify(Callback(status=1, description="Some fields required!").dict())
-    
-    usrManager = UserManager()
-    callback   = usrManager.AddUser(username, email, password)
-
-    logger.debug("Register user: end")
-    return jsonify(callback.dict())
+        logger.debug("Register user: end")
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @userBlueprint.route('/user', methods=['PATCH'])
 @cross_origin()
 @loginRequired(cfg.ROLES.ADMIN)
 def setUserRole():
-    logger.debug("Update user: begin")
-    
-    if "data" not in request.json:
-        logger.warn("'data' field needed")
-        return jsonify(Callback(status=1, description="'data' field required").dict())
-    data     = request.json["data"]
+    try:
+        if "data" not in request.json:
+            logger.warn("'data' field needed")
+            return jsonify(Callback(status=1, description="'data' field required").dict())
+        data     = request.json["data"]
 
-    username = data["name"]
-    newRole  = data["role"]
+        username = data["name"]
+        newRole  = data["role"]
 
-    logger.debug("CHECK BEGIN")
-    logger.debug(data)
-    logger.debug(username)
-    logger.debug(newRole)
-    logger.debug("CHECK END")
+        usrManager = UserManager()
+        userStatus = usrManager.GetUser(username=username)
+        if userStatus.status != 0:
+            userStatus.data = None
+            return jsonify(userStatus.dict())
+        
+        user       = userStatus.data
+        callback   = usrManager.SetRole(user, newRole)
 
-    usrManager = UserManager()
-    userStatus = usrManager.GetUser(username=username)
-    if userStatus.status != 0:
-        userStatus.data = None
-        return jsonify(userStatus.dict())
-    
-    user       = userStatus.data
-    callback   = usrManager.SetRole(user, newRole)
-
-    logger.debug("Update user: end")
-    return jsonify(callback.dict())
+        logger.debug("Update user: end")
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @userBlueprint.route('/user', methods=['DELETE'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def removeUser():
-    logger.debug("Remove user: begin")
-    
-    sid = request.args.get('sid')
+    try:
+        sid = request.args.get('sid')
 
-    usrManager = UserManager()
+        usrManager = UserManager()
 
-    userStatus = usrManager.GetUser(sid=sid)
-    user       = userStatus.data
+        userStatus = usrManager.GetUser(sid=sid)
+        user       = userStatus.data
 
-    callback   = usrManager.RemoveUser(user)
+        callback   = usrManager.RemoveUser(user)
 
-    logger.debug("Remove user: end")
-    return jsonify(callback.dict())
+        logger.debug("Remove user: end")
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
 @userBlueprint.route('/users', methods=['GET'])
 @cross_origin()
 @loginRequired(cfg.ROLES.STUDENT)
 def userList():
-    logger.debug("List users: begin")
-
-    usrManager = UserManager()
-    callback   = usrManager.ListUsers()
-    
-    logger.debug("List users: end")
-    return jsonify(callback.dict())
+    try:
+        usrManager = UserManager()
+        callback   = usrManager.ListUsers()
+        
+        logger.debug("List users: end")
+        return jsonify(callback.dict())
+    except Exception as e:
+        logger.error("Caught exception at {endpoint}".format(endpoint=request.endpoint))
+        logger.exception(e)
+        return jsonify(Callback(100, "Unexpected error").dict())
 
